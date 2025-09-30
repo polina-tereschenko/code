3543ea3b1211988238238a60c1bd04ff0f83af3c
Polina Tereschenko <polinatereschenko16@gmail.com>
2025-09-30 13:15:01 +0300

    Add posts & home page

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Get started
 
 ```shell
+python -m venv .venv
+source .venv/bin/activate
 python manage.py runserver
 ```
--- message_board/message_board/settings.py
+++ message_board/message_board/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        "DIRS": [BASE_DIR / "templates"],
     },
 ]
 
