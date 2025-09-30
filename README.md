16e7cbbabb4f3e68613058f7866b78d8f88a72a8
Polina Tereschenko <polinatereschenko16@gmail.com>
2025-09-30 16:19:12 +0300

    Add table

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ python -m venv .venv
 source .venv/bin/activate
 python manage.py runserver
 ```
+
+## Run tests
+
+```shell
+python manage.py test
+```
--- message_board/posts/models.py
+++ message_board/posts/models.py
@@ -1,6 +1,16 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib import admin
 
 class Post(models.Model):
     text = models.TextField()
+    data = models.DateField(default=timezone.now)
+    temperature = models.FloatField(default=0.0)
+    preseure = models.IntegerField(default=0)
+    humidity = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.text[:50]
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["text", "data", "temperature", "preseure", "humidity"]
