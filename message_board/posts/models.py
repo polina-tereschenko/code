269b1479372cdb7bf9e92afb05822250a8202463
Polina Tereschenko <polinatereschenko16@gmail.com>
2025-11-25 04:25:17 +0200

    Add tests & template

--- message_board/posts/models.py
+++ message_board/posts/models.py
@@ -6,11 +6,11 @@ class Post(models.Model):
     text = models.TextField()
     data = models.DateField(default=timezone.now)
     temperature = models.FloatField(default=0.0)
-    preseure = models.IntegerField(default=0)
+    pressure = models.IntegerField(default=0)
     humidity = models.IntegerField(default=0)
     
     def __str__(self):
         return self.text[:50]
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["text", "data", "temperature", "preseure", "humidity"]
+    list_display = ["text", "data", "temperature", "pressure", "humidity"]
