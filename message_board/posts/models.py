3a17c4829d518fa1ab9930fdb44608501514d4a9
Polina Tereschenko <polinatereschenko16@gmail.com>
2025-12-18 01:45:51 +0200

    Add forms

--- message_board/posts/models.py
+++ message_board/posts/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.utils import timezone
 from django.db import models
 from django.contrib import admin
@@ -8,9 +9,13 @@ class Post(models.Model):
     temperature = models.FloatField(default=0.0)
     pressure = models.IntegerField(default=0)
     humidity = models.IntegerField(default=0)
+    wind_speed = models.IntegerField(default=0)
     
     def __str__(self):
         return self.text[:50]
+    
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={"pk": self.pk})
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["text", "data", "temperature", "pressure", "humidity"]
+    list_display = ["text", "data", "temperature", "pressure", "humidity", "wind_speed"]
